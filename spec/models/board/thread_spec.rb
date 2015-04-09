fd408264bc9a24bb93ce08aa6416c5279829f279
sert <demo.sert@gmail.com>
2015-04-09 22:29:52 +0900

    use let function

--- spec/models/board/thread_spec.rb
+++ spec/models/board/thread_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Board::Thread, type: :model, dbscope: :example do
-  it "isn't valid without title and body" do
-    thread = Board::Thread.new
-    thread.title = "title"
-    thread.body = "body"
-    expect(thread).to be_valid
+  describe "valid" do
+    let(:thread) { Board::Thread.new body: 'body', title: 'title' }
+
+    it "isn't valid without title and body" do
+      expect(thread).to be_valid
+    end
   end
 
-  it "isn't valid without title" do
-    thread = Board::Thread.new
-    thread.title = nil
-    thread.body = "body"
-    expect(thread).not_to be_valid
+  describe "invalid" do
+    context "without body" do
+      let(:thread) { Board::Thread.new title: 'title' }
+      it { expect(thread).not_to be_valid }
+    end
+
+    context "without title" do
+      let(:thread) { Board::Thread.new body: 'body' }
+      it { expect(thread).not_to be_valid }
+    end
   end
 end
