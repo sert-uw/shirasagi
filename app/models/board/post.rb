457b9a142b6edc3cfb640455c497696bcbb76873
ka <ka.kaosf@gmail.com>
2015-05-10 15:03:08 +0900

    Implement descendants_updated field

--- app/models/board/post.rb
+++ app/models/board/post.rb
@@ -7,4 +7,19 @@ class Board::Post
   belongs_to :parent, class_name: "Board::Post"
 
   has_many :children, class_name: "Board::Post", dependent: :destroy
+
+  before_save :set_descendants_updated
+  after_save :update_parent_descendants_updated
+
+  def set_descendants_updated
+    self.descendants_updated = updated
+  end
+
+  def update_parent_descendants_updated(time = nil)
+    if parent.present?
+      time ||= descendants_updated
+      parent.set descendants_updated: time
+      parent.update_parent_descendants_updated time
+    end
+  end
 end
--- spec/models/board/post_spec.rb
+++ spec/models/board/post_spec.rb
@@ -71,4 +71,65 @@ RSpec.describe Board::Post, type: :model, dbscope: :example do
       end
     end
   end
+
+  describe "descendants_updated field" do
+    describe "set before save" do
+      context "when creating" do
+        let(:post) { build :board_post }
+        it do
+          expect { post.save }
+            .to change { post.descendants_updated }.from(nil)
+        end
+
+        describe "it is same to updated field" do
+          before { post.save }
+          it { expect(post.descendants_updated).to eq_as_time post.updated }
+        end
+      end
+
+      context "when updating" do
+        let(:post) { create :board_post }
+        before { post.body += 'added text' }
+        it { expect { post.save }.to change { post.descendants_updated } }
+
+        describe "it is same to updated field" do
+          before { post.save }
+          it { expect(post.descendants_updated).to eq_as_time post.updated }
+        end
+      end
+    end
+
+    describe "of parent post" do
+      let!(:topic_id) { create(:board_topic).id }
+
+      def topic
+        described_class.find topic_id
+      end
+
+      context "when creating a comment" do
+        it do
+          expect { create :board_comment, parent: topic }
+            .to change { topic.descendants_updated }
+        end
+      end
+
+      context "when creating a comment to comment" do
+        let!(:comment_id) { create(:board_comment, parent: topic).id }
+
+        def comment
+          described_class.find comment_id
+        end
+
+        it do
+          expect { create :board_comment, parent: comment }
+            .to change { comment.descendants_updated }
+        end
+
+        it do
+          expect { create :board_comment, parent: comment }
+            .to change { topic.descendants_updated }
+        end
+      end
+    end
+  end
 end
