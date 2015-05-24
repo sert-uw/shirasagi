5c4ab8f767b21c8ae04054e5d1e88341750253b3
sert <demo.sert@gmail.com>
2015-05-24 21:45:53 +0900

    add authority check (topics_controller)

--- app/controllers/board/topics_controller.rb
+++ app/controllers/board/topics_controller.rb
@@ -11,6 +11,14 @@ class Board::TopicsController < ApplicationController
     @comments = @topic.children
   end
 
+  def edit
+    if @topic.user == @cur_user
+      render :edit
+    else
+      redirect_to board_topics_url, notice: t('board.no_authority')
+    end
+  end
+
   def new
     @topic = Board::Post.new
   end
@@ -34,8 +42,12 @@ class Board::TopicsController < ApplicationController
   end
 
   def destroy
-    @topic.destroy
-    redirect_to board_topics_url, notice: t('board.topic.notice.delete')
+    if @topic.user == @cur_user
+      @topic.destroy
+      redirect_to board_topics_url, notice: t('board.topic.notice.delete')
+    else
+      redirect_to board_topics_url, notice: t('board.no_authority')
+    end
   end
 
   private
